f6c3efea9eeee8585c4f079b1fe24c8725704cf1
Mariusz Zaborski <oshogbo@FreeBSD.org>
2015-07-22 19:10:26 +0200

    Add script to run multiple time test.

---
+++ stress_packunpack.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright (c) 2015 Mariusz Zaborski <oshogbo@FreeBSD.org>
+#
+
+PROG=bin/packunpack
+
+if [ \! -x "${PROG}" ]; then
+	echo "First use gmake to compile tools." >&2
+	exit 1
+fi
+
+if [ $# -ne 3 ]; then
+	echo "Usage: ./$0 [min-nvlist-size] [max-nvlist-size] [count]" >&2
+	exit 1
+fi
+
+i=0
+while [ ${i} -ne ${3} ]; do
+	./${PROG} `jot -r 1 "${1}" "${2}"`
+	if [ $? -ne 0 ]; then
+		echo "Error detected."
+		exit 1
+	fi
+
+	i=$((i+1))
+done
+
