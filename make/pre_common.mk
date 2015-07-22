b1b2309fc673396556f2814418bec42881d99540
Mariusz Zaborski <oshogbo@FreeBSD.org>
2015-07-23 01:23:52 +0200

    Be brutal with includes.

--- make/pre_common.mk
+++ make/pre_common.mk
@@ -4,11 +4,11 @@
 
 LIBNV?=/usr/src/lib/libnv
 LIBNVCONTRIB?=/usr/src/sys/contrib/libnv
-INCLIBNV?=/usr/local/include/sys
+INCLIBNV?=/usr/include
 
 CC=	atf-gcc
 LIBS=
 BINDIR=	bin
 SRCDIR=	src
 OBJDIR=	obj
-CFLAGS=	-Wall -Wextra -I${INCLIBNV}
+CFLAGS=	-Wall -Wextra -I${INCLIBNV} -I${INCLIBNV}/sys
