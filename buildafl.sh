1d15c1bc24e88c5f419dcd0eff6e93175ab5fc0a
Mariusz Zaborski <oshogbo@FreeBSD.org>
2015-07-22 23:10:22 +0200

    Add script to fetch and build afl.

---
+++ buildafl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+fetch "http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz"
+tar -xvf afl-latest.tgz
+dir=`tar -tf afl-latest.tgz | head -n1`
+rm afl-latest.tgz
+gmake -C "${dir}"
+
+
